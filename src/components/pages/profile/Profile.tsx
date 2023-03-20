0f0fccf0b7e6580354939fadf20cd23055d0c8f2
Koichi-hub <gogotimur2020@gmail.com>
2023-03-20 15:29:59 +0500

    Profile & ProjectCard
    Улучшена разметка и стили, добавлены примеры

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -2,7 +2,7 @@ import './Profile.scss';
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TelegramIcon from '@mui/icons-material/Telegram';
-import { Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import dayjs from 'dayjs';
 
 import { ProgLangs } from '../../../common/types/ProgLangs';
@@ -11,13 +11,44 @@ import ProjectCard from '../../project_card/ProjectCard';
 const data = {
   telegram: 'link',
   github: 'link',
+  username: 'Имя пользователя',
+  progLangs: [ProgLangs.JavaScript] as ProgLangs[],
+  description: 'Какое-то описание',
   projects: [
     {
-      title: 'Взлом жепы',
-      description: 'Мы собираем команду для взлома жопы',
+      title: 'Проект 1',
+      description: 'Описание к проекту 1',
       progLangs: [ProgLangs.C, ProgLangs.Go, ProgLangs.TypeScript] as ProgLangs[],
       usersNumber: 1,
       date: dayjs().format('DD.MM.YYYY'),
+    },
+    {
+      title: 'Проект 2',
+      description: 'Описание к проекту 2',
+      progLangs: [ProgLangs.C, ProgLangs.Go, ProgLangs.TypeScript] as ProgLangs[],
+      usersNumber: 1,
+      date: dayjs().subtract(1, 'day').format('DD.MM.YYYY'),
+    },
+    {
+      title: 'Проект 3',
+      description: 'Описание к проекту 3',
+      progLangs: [ProgLangs.C, ProgLangs.Go, ProgLangs.TypeScript] as ProgLangs[],
+      usersNumber: 1,
+      date: dayjs().subtract(2, 'day').format('DD.MM.YYYY'),
+    },
+    {
+      title: 'Проект 4',
+      description: 'Описание к проекту 4',
+      progLangs: [ProgLangs.C, ProgLangs.Go, ProgLangs.TypeScript] as ProgLangs[],
+      usersNumber: 1,
+      date: dayjs().subtract(3, 'day').format('DD.MM.YYYY'),
+    },
+    {
+      title: 'Проект 5',
+      description: 'Описание к проекту 5',
+      progLangs: [ProgLangs.C, ProgLangs.Go, ProgLangs.TypeScript] as ProgLangs[],
+      usersNumber: 1,
+      date: dayjs().subtract(4, 'day').format('DD.MM.YYYY'),
     },
   ],
 };
@@ -31,8 +62,8 @@ const Profile = () => {
         <div className='profile-info-top_part'>
           <div className='profile-info-top_part-username'>
             <div className='profile-info-top_part-username-text marquee'>
-              <span>Очень длинная строка прям ну вообще помогити</span>
-              <span>Очень длинная строка прям ну вообще помогити</span>
+              <span>{data.username}</span>
+              <span>{data.username}</span>
             </div>
           </div>
           <div className='profile-info-top_part-logo'>
@@ -42,11 +73,13 @@ const Profile = () => {
             />
           </div>
           <div className='profile-info-top_part-prog_langs'>
-            <span>prog_langs</span>
+            <span>{data.progLangs}</span>
           </div>
         </div>
-        <div className='profile-info-description'>kek</div>
-        <div className='profile-info-buttons'>
+        <div className='profile-info-description'>{data.description}</div>
+        <Stack
+          spacing={1}
+          direction='row'>
           <Button
             variant='contained'
             startIcon={<TelegramIcon />}
@@ -59,9 +92,16 @@ const Profile = () => {
             className='profile-info-buttons-github'>
             <span className='profile-info-buttons-github-text'>{data.github}</span>
           </Button>
-        </div>
+        </Stack>
       </div>
       <div className='profile-projects'>
+        <ProjectCard
+          title='Новый проект'
+          description='Создать новый проект'
+          usersNumber='?'
+          date={dayjs().format('DD.MM.YYYY')}
+          className='new_project_card'
+        />
         {data.projects.map(({ title, description, progLangs, usersNumber, date }: Data['projects'][0], i) => (
           <ProjectCard
             title={title}
