3be1be75d3dc4da51b350e11f5ec0980dfafa5f6
Koichi-hub <gogotimur2020@gmail.com>
2023-03-20 15:29:59 +0500

    Profile & ProjectCard
    Улучшена разметка и стили, добавлены примеры

--- src/components/project_card/ProjectCard.tsx
+++ src/components/project_card/ProjectCard.tsx
@@ -1,39 +1,45 @@
 import './ProjectCard.scss';
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useMemo } from 'react';
 
 import { ProgLangs } from '../../common/types/ProgLangs';
 
 type Props = {
   title: string;
-  description: string;
-  progLangs: ProgLangs[];
-  usersNumber: number;
+  description?: string;
+  progLangs?: ProgLangs[];
+  usersNumber: number | string;
   date: string;
   onClick?: () => void;
+  className?: 'new_project_card';
 };
 
-const ProjectCard = ({ title, description, progLangs, usersNumber, date, onClick }: Props) => {
+const ProjectCard = ({ title, description, progLangs, usersNumber, date, onClick, className }: Props) => {
+  const renderProgLangs = useMemo(
+    () =>
+      progLangs?.map((_, i) => (
+        <img
+          className='project_card-right_part-prog_langs-icon'
+          key={i}
+        />
+      )),
+    [progLangs]
+  );
+
   return (
     <div
-      className='project_card'
+      className={['project_card', className].join(' ')}
       onClick={onClick}>
       <div className='project_card-left_part'>
         <span className='project_card-left_part-title'>{title}</span>
         <span className='project_card-left_part-description'>{description}</span>
       </div>
       <div className='project_card-right_part'>
-        <div className='project_card-right_part-prog_langs'>
-          {progLangs.map((_, i) => (
-            <img
-              className='project_card-right_part-prog_langs-icon'
-              key={i}
-            />
-          ))}
-        </div>
+        <div className='project_card-right_part-prog_langs'>{renderProgLangs}</div>
         <div className='project_card-right_part-users_number'>
           <span className='project_card-right_part-users_number-text'>{usersNumber}</span>
-          <AccountCircleIcon />
+          <AccountCircleIcon sx={{ fontSize: 20 }} />
         </div>
         <div className='project_card-right_part-date'>{date}</div>
       </div>
