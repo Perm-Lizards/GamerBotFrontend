1675c77f2c14e90acc50cecfd220b94192841206
neiwan <neiwan@mail.ru>
2023-03-24 15:51:35 +0500

    Добавлена строка поиска и фильтрации проектов
    Добавлены разметка и стили

--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -1,10 +1,30 @@
+import './Projects.scss';
+
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton, Stack } from '@mui/material';
+
 import Layout from '../../layout/Layout';
 
 const Projects = () => {
   return (
     <Layout>
-      <div>jej</div>
-    </Layout>
+      <div className="projects">
+        <div className='projects-search'>
+          <Stack
+            justifyContent={'right'}
+            spacing={1}
+            direction='row'>
+            <IconButton className='profiles-buttons-filter'>
+              <FilterAltIcon />
+            </IconButton>
+            <IconButton className='profiles-buttons-search'>
+              <SearchIcon />
+            </IconButton>
+          </Stack>
+        </div>
+      </div>
+    </Layout >
   );
 };
 
