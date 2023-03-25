98e9e109d0cadd7c36fde73877df51d4e3e36c4e
Koichi-hub <gogotimur2020@gmail.com>
2023-03-25 21:49:12 +0500

    Обновлена маршрутизация

--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -4,27 +4,23 @@ import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton, Stack } from '@mui/material';
 
-import Layout from '../../layout/Layout';
-
 const Projects = () => {
   return (
-    <Layout>
-      <div className="projects">
-        <div className='projects-search'>
-          <Stack
-            justifyContent={'right'}
-            spacing={1}
-            direction='row'>
-            <IconButton className='profiles-buttons-filter'>
-              <FilterAltIcon />
-            </IconButton>
-            <IconButton className='profiles-buttons-search'>
-              <SearchIcon />
-            </IconButton>
-          </Stack>
-        </div>
+    <div className='projects'>
+      <div className='projects-search'>
+        <Stack
+          justifyContent={'right'}
+          spacing={1}
+          direction='row'>
+          <IconButton className='profiles-buttons-filter'>
+            <FilterAltIcon />
+          </IconButton>
+          <IconButton className='profiles-buttons-search'>
+            <SearchIcon />
+          </IconButton>
+        </Stack>
       </div>
-    </Layout >
+    </div>
   );
 };
 
