7e15e6b582404c30fb7e278091b1ecd6265051c7
Koichi-hub <gogotimur2020@gmail.com>
2023-03-25 21:51:06 +0500

    Обновлены типы

--- src/common/types/ProjectType.ts
+++ src/common/types/ProjectType.ts
@@ -1,11 +1,12 @@
 import { ProgLangsEnum } from '../enums/ProgLangsEnum';
 
 export type ProjectType = {
+  id?: number;
   github?: string;
   telegram?: string;
-  title: string;
-  description: string;
-  progLangs: ProgLangsEnum[];
-  usersNumber: number;
-  createdAt: string;
+  title?: string;
+  description?: string;
+  progLangs?: ProgLangsEnum[];
+  usersNumber?: number;
+  createdAt?: string;
 };
--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -8,7 +8,7 @@ import dayjs from 'dayjs';
 import { useMemo } from 'react';
 
 import { ProgLangsEnum } from '../../../common/enums/ProgLangsEnum';
-import Layout from '../../layout/Layout';
+import { ProjectType } from '../../../common/types/ProjectType';
 import ProgLangs from '../../prog_langs/ProgLangs';
 import ProjectCard from '../../project_card/ProjectCard';
 import CreateProjectPopup from './components/create_project_popup/CreateProjectPopup';
@@ -24,6 +24,7 @@ const profile = {
   isOwner: true,
   projects: [
     {
+      id: 1,
       title: 'Проект 1',
       description: 'Описание к проекту 1',
       progLangs: [ProgLangsEnum.C, ProgLangsEnum.Go, ProgLangsEnum.TypeScript] as ProgLangsEnum[],
@@ -31,6 +32,7 @@ const profile = {
       createdAt: dayjs().format('DD.MM.YYYY'),
     },
     {
+      id: 2,
       title: 'Проект 2',
       description: 'Описание к проекту 2',
       progLangs: [ProgLangsEnum.C, ProgLangsEnum.Go, ProgLangsEnum.TypeScript] as ProgLangsEnum[],
@@ -38,6 +40,7 @@ const profile = {
       createdAt: dayjs().subtract(1, 'day').format('DD.MM.YYYY'),
     },
     {
+      id: 3,
       title: 'Проект 3',
       description: 'Описание к проекту 3',
       progLangs: [ProgLangsEnum.C, ProgLangsEnum.Go, ProgLangsEnum.TypeScript] as ProgLangsEnum[],
@@ -45,6 +48,7 @@ const profile = {
       createdAt: dayjs().subtract(2, 'day').format('DD.MM.YYYY'),
     },
     {
+      id: 4,
       title: 'Проект 4',
       description: 'Описание к проекту 4',
       progLangs: [ProgLangsEnum.C, ProgLangsEnum.Go, ProgLangsEnum.TypeScript] as ProgLangsEnum[],
@@ -52,13 +56,14 @@ const profile = {
       createdAt: dayjs().subtract(3, 'day').format('DD.MM.YYYY'),
     },
     {
+      id: 5,
       title: 'Проект 5',
       description: 'Описание к проекту 5',
       progLangs: [ProgLangsEnum.C, ProgLangsEnum.Go, ProgLangsEnum.TypeScript] as ProgLangsEnum[],
       usersNumber: 1,
       createdAt: dayjs().subtract(4, 'day').format('DD.MM.YYYY'),
     },
-  ],
+  ] as ProjectType[],
 };
 
 const Profile = () => {
@@ -74,8 +79,9 @@ const Profile = () => {
 
   const renderProjects = useMemo(
     () =>
-      profile.projects.map(({ title, description, progLangs, usersNumber, createdAt }, i) => (
+      profile.projects.map(({ id, title, description, progLangs, usersNumber, createdAt }, i) => (
         <ProjectCard
+          id={id}
           title={title}
           description={description}
           progLangs={progLangs}
@@ -88,51 +94,49 @@ const Profile = () => {
   );
 
   return (
-    <Layout>
-      <div className='profile'>
-        {renderEditProfilePopup}
-        <div className='profile-info'>
-          <div className='profile-info-top_part'>
-            <Username username={profile.username} />
-            <div className='profile-info-top_part-logo'>
-              <AccountCircleIcon />
-            </div>
-            <div className='profile-info-top_part-prog_langs'>
-              <ProgLangs
-                progLangs={profile.progLangs}
-                width={30}
-                height={30}
-              />
-            </div>
+    <div className='profile'>
+      {renderEditProfilePopup}
+      <div className='profile-info'>
+        <div className='profile-info-top_part'>
+          <Username username={profile.username} />
+          <div className='profile-info-top_part-logo'>
+            <AccountCircleIcon />
           </div>
-          <div className='profile-info-description'>{profile.description}</div>
-          <Stack
-            spacing={1}
-            direction='row'>
-            <Button
-              variant='contained'
-              startIcon={<TelegramIcon />}
-              className='profile-info-buttons-telegram'>
-              <span className='profile-info-buttons-telegram-text'>{profile.telegram}</span>
-            </Button>
-            <Button
-              variant='contained'
-              startIcon={<GitHubIcon />}
-              className='profile-info-buttons-github'>
-              <span className='profile-info-buttons-github-text'>{profile.github}</span>
-            </Button>
-          </Stack>
+          <div className='profile-info-top_part-prog_langs'>
+            <ProgLangs
+              progLangs={profile.progLangs}
+              width={30}
+              height={30}
+            />
+          </div>
         </div>
-        <div className='profile-projects'>
-          <Stack
-            gap='5px'
-            padding='0 10px'>
-            {renderProjects}
-          </Stack>
-          <CreateProjectPopup />
-        </div>
+        <div className='profile-info-description'>{profile.description}</div>
+        <Stack
+          spacing={1}
+          direction='row'>
+          <Button
+            variant='contained'
+            startIcon={<TelegramIcon />}
+            className='profile-info-buttons-telegram'>
+            <span className='profile-info-buttons-telegram-text'>{profile.telegram}</span>
+          </Button>
+          <Button
+            variant='contained'
+            startIcon={<GitHubIcon />}
+            className='profile-info-buttons-github'>
+            <span className='profile-info-buttons-github-text'>{profile.github}</span>
+          </Button>
+        </Stack>
       </div>
-    </Layout>
+      <div className='profile-projects'>
+        <Stack
+          gap='5px'
+          padding='0 10px'>
+          {renderProjects}
+        </Stack>
+        <CreateProjectPopup />
+      </div>
+    </div>
   );
 };
 
